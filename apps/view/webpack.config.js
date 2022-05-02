0af7264a39c25e9ecd94dc57a6fcd179f7f63b94
beast462 <binhmattroi@gmail.com>
2022-05-02 11:02:39 +0700

    Add initial components

---
+++ apps/view/webpack.config.js
@@ -0,0 +1,172 @@
+/* eslint-disable */
+
+const CopyPlugin = require('copy-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const { join, resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const tsConfig = require('./tsconfig.app.json');
+const dependencies = require('../../package.json').dependencies;
+
+const __root = resolve(__dirname, '..', '..');
+const isDev = process.argv.includes('--dev');
+const outDir = resolve(__dirname, tsConfig.compilerOptions.outDir);
+const commonLibDir = resolve(__root, 'libs', 'common', 'src');
+
+function getDep(name) {
+  return dependencies[name].replace('^', '').trim();
+}
+
+const prodDependencies = {
+  react: [
+    `https://unpkg.com/react@${getDep('react')}/umd/react.production.min.js`,
+    'global',
+    'crossorigin',
+    'anonymous',
+  ],
+  'react-dom': [
+    `https://unpkg.com/react-dom@${getDep(
+      'react-dom',
+    )}/umd/react-dom.production.min.js`,
+    'global',
+    'crossorigin',
+    'anonymous',
+  ],
+};
+
+const config = {
+  entry: join(__dirname, 'src', 'index.tsx'),
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    modules: [
+      join(__dirname, 'src'),
+      commonLibDir,
+      join(__root, 'node_modules'),
+    ],
+    alias: {
+      '@/view': resolve(__dirname, 'src'),
+      '@/common': commonLibDir,
+    },
+  },
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  context: __dirname,
+  mode: isDev ? 'development' : 'production',
+  devtool: isDev ? 'source-map' : false,
+  output: {
+    path: outDir,
+    publicPath: '/',
+    filename: 'main.js',
+  },
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-typescript',
+              [
+                '@babel/preset-react',
+                {
+                  runtime: 'automatic',
+                },
+              ],
+            ],
+            plugins: [
+              '@babel/plugin-transform-runtime',
+              '@babel/plugin-transform-typescript',
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-proposal-nullish-coalescing-operator',
+              '@babel/plugin-proposal-optional-chaining',
+              '@babel/plugin-transform-async-to-generator',
+              [
+                '@babel/plugin-transform-react-jsx',
+                {
+                  runtime: 'automatic',
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpg|gif|webp|woff|woff2)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(svg)$/,
+        use: ['svg-inline-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  externalsType: 'script',
+  externals: isDev ? {} : prodDependencies,
+  optimization: {
+    minimize: !isDev,
+    minimizer: [
+      new TerserWebpackPlugin({
+        test: /\.js(\?.*)?$/,
+        parallel: 1,
+        terserOptions: {
+          compress: true,
+          sourceMap: false,
+          format: {
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: join(__dirname, 'index.html'),
+      inject: true,
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: join(__dirname, 'public'),
+          to: join(outDir, 'public'),
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
+};
+
+const devServer = {
+  compress: false,
+  hot: true,
+  static: false,
+  open: false,
+  devMiddleware: {
+    writeToDisk: true,
+  },
+};
+
+if (isDev) {
+  const portfinder = require('portfinder');
+  const DevServer = require('webpack-dev-server');
+  const compiler = require('webpack')(config);
+
+  portfinder.getPort((err, port) => {
+    if (err) throw err;
+
+    const server = new DevServer({ ...devServer, port }, compiler);
+
+    server.start();
+  });
+}
+
+module.exports = config;
