24733973902f05f4befdab4446000186fb96e047
beast462 <binhmattroi@gmail.com>
2022-05-18 10:57:58 +0700

    Add dns over tls package

--- libs/common/src/models/fallback-address.ts
+++ libs/common/src/models/fallback-address.ts
@@ -1,6 +1,8 @@
+export const FallbackProtocols = ['udp', 'tcp', 'tls', 'https', 'http'];
+
 export type FallbackAddress = {
   host: string;
   port: number;
   path?: string;
-  proto: 'http' | 'https' | 'tls' | 'udp';
+  proto: 'http' | 'https' | 'tls' | 'tcp' | 'udp';
 };
