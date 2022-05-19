552476955dc3f3a83f5b9c62b134bbb3c521ccac
beast462 <binhmattroi@gmail.com>
2022-05-19 21:59:03 +0700

    Add route on /manage/:domain

--- apps/view/src/components/Dashboard/ManagerPane/index.tsx
+++ apps/view/src/components/Dashboard/ManagerPane/index.tsx
@@ -3,7 +3,9 @@ import { Route } from 'react-router';
 import ManagerPane, { meta } from './ManagerPane';
 
 export const route = (
-  <Route path={meta.path.replace(/^\//, '')} element={<ManagerPane />} />
+  <Route path={meta.path.replace(/^\//, '')} element={<ManagerPane />}>
+    <Route path=":domain" element={<ManagerPane />} />
+  </Route>
 );
 
 export * from './ManagerPane';
