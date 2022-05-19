9784a8d05b488ecb7a88ef89b6bedf9a0311b464
beast462 <binhmattroi@gmail.com>
2022-05-20 00:40:02 +0700

    Update domain list item: omit ownerId

--- libs/common/src/models/domain-list-item.ts
+++ libs/common/src/models/domain-list-item.ts
@@ -1,5 +1,5 @@
 import { DomainEntity } from '../entities';
 
-export type DomainListItem = Omit<DomainEntity, 'owner'> & {
+export type DomainListItem = Omit<DomainEntity, 'owner' | 'ownerId'> & {
   records: number;
 };
