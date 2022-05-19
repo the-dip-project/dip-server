9c8b8df4787c04d77350c460182539320f9603b2
beast462 <binhmattroi@gmail.com>
2022-05-19 07:40:15 +0700

    Add webfontloader

--- apps/view/src/index.tsx
+++ apps/view/src/index.tsx
@@ -1,11 +1,19 @@
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import WebFont from 'webfontloader';
+
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
 import App from './App';
 import store from './store/store';
+
+WebFont.load({
+  google: {
+    families: ['Roboto:300,400,500'],
+  },
+});
 
 render(
   <React.StrictMode>
