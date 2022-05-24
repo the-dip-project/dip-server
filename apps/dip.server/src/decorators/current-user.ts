bdba81d08d2ede71d431fe7c2f190d110f33ec78
beast462 <binhmattroi@gmail.com>
2022-05-25 01:27:18 +0700

    Handle null in current user decorator

--- apps/dip.server/src/decorators/current-user.ts
+++ apps/dip.server/src/decorators/current-user.ts
@@ -5,10 +5,10 @@ interface IProps {
 }
 
 export const CurrentUser = createParamDecorator(
-  ({ escalation }: IProps, ctx: ExecutionContext) => {
+  ({ escalation }: IProps = { escalation: false }, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
 
     if (!escalation) return request.user;
-    else return { ...request.user, escalatedUntil: request.escalatedUntil };
+    else return { ...request.user, escalatedUntil: request?.escalatedUntil };
   },
 );
